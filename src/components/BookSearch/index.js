2305a8e44da8a3b16548e8f42af727a07f280ab6
Timothy Smith <timothytsmith8@gmail.com>
2021-02-24 22:04:21 -0800

    Basic navbar and BookSearch components created. (Will be updating these later.)

---
+++ src/components/BookSearch/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./BookSearch.css";
+
+function BookSearch() {
+  return (
+    <header className="header">
+     
+      <h1> (React) Google Books Search </h1>
+    </header>
+  );
+}
+
+export default BookSearch;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css"
+function Nav() {
+    return (
+        <div>
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+      </nav>
+      </div>
+    );
+}
+
+    
+    export default Nav
